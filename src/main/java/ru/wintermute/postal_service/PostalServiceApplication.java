77a65f399c32f7ec89c22e7d57c1be97826d9185
Wintermute <williwaw92@gmail.com>
2025-01-31 15:13:55 +0300

    very beginning

---
+++ src/main/java/ru/wintermute/postal_service/PostalServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.wintermute.postal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/wintermute/postal_service/PostalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wintermute.postal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
