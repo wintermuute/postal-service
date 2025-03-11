eb570981362188acb77e6a814fabce1fa057860f
Wintermute <ya.williwaw@yandex.ru>
2025-03-11 13:46:46 +0300

    pool

--- src/main/java/ru/wintermute/postal_service/config/DataSourceConfig.java
+++ src/main/java/ru/wintermute/postal_service/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package ru.wintermute.postal_service.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,17 +12,21 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    //@Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+//        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postal");
+//        dataSourceBuilder.username("postgres");
+//        dataSourceBuilder.password("postgres");
+//        return dataSourceBuilder.build();
+//    }
+@Bean
+@ConfigurationProperties("spring.datasource")
+public DataSource hikariDataSource() {
+    return DataSourceBuilder.create().type(HikariDataSource.class).build();
+}
     @Bean
-    @ConfigurationProperties("spring.datasource")
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postal");
-        dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("postgres");
-        return dataSourceBuilder.build();
-    }
-    @Bean
-    public JdbcTemplate jdbcTemplate() {return  new JdbcTemplate(getDataSource());}
+    public JdbcTemplate jdbcTemplate() {return  new JdbcTemplate(hikariDataSource());}
 
 }
